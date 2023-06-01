8d45aa424109908ed5708538beba6953dd1b4ae6
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-06-01 10:20:10 -0700

    Second commit
    Displaying all posts

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -1,5 +1,11 @@
 import { useSelector } from 'react-redux';
 import { selectAllUsers } from '../users/usersSlice';
 
-// const PostAuthor = ({ userId }) => {};
-// export default PostAuthor;
+const PostAuthor = ({ userId }) => {
+  const users = useSelector(selectAllUsers);
+
+  // const author = users.find((user) => user.id === userId);
+  return <span>by {'Unkown author'}</span>;
+};
+
+export default PostAuthor;
--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -1,4 +1,17 @@
-// import { parseISO, formatDistanceToNow } from 'date-fns';
+import { parseISO, formatDistanceToNow } from 'date-fns';
 
-// const TimeAgo = ({ timestamp }) => {};
-// export default TimeAgo;
+const TimeAgo = ({ timestamp }) => {
+  let timeAgo = '';
+  if (timestamp) {
+    const date = parseISO(timestamp);
+    const timePeriod = formatDistanceToNow(date);
+    timeAgo = `${timePeriod} ago`;
+  }
+
+  return (
+    <span>
+      &nbsp; <i>{timeAgo}</i>
+    </span>
+  );
+};
+export default TimeAgo;
